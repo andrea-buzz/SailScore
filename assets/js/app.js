8fc99c952392fa578e779b28104fb6d2ac8f6187
andrea-buzz <andrea-buzz@github.com>
2022-12-26 11:27:02 +0000

    pull top

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,4 +10,9 @@ let competitors = [
   {name: "Max Cinquepalmi", sail_number: "183555", boat_class: "ILCA 6"},
 ];
 let tag_competitors = document.querySelector('[data-list="competitors"]');
-competitors.forEach(c => tag_competitors.insertAdjacentHTML('afterBegin',`<div class="competitor" data-status="started"> <button type="button" data-role="pull-top">&#8679;</button> <span>${c.name}</span> <span>${c.sail_number}</span> <span>${c.boat_class}</span> <button type="button" data-role="set-arrived">&#9201;</button></div>`));;
+competitors.forEach(c => tag_competitors.insertAdjacentHTML('afterBegin',`<div class="competitor" data-status="started"> <button type="button" data-role="pull-top">&#8679;</button> <span>${c.name}</span> <span>${c.sail_number}</span> <span>${c.boat_class}</span> <button type="button" data-role="set-arrived">&#9201;</button></div>`));
+document.querySelectorAll('[data-role="pull-top"]').forEach(b => b.addEventListener('click', pull_top));
+function pull_top (e){
+  let c = e.currentTarget.parentNode; 
+  c.style.order --;
+}
