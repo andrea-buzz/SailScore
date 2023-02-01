b7d350e473cb246bbc47c2d4c173421bef221475
andrea-buzz <andrea-buzz@github.com>
2023-02-01 00:16:27 +0000

    FIX salvataggio f_club quando non  definito

--- assets/js/app.js
+++ assets/js/app.js
@@ -1568,7 +1568,7 @@ function save_regatta(e){
   
   regatta._setFromArray = ff;
   regatta.UsePY = regatta.usePY;
-  if(f_club.dataset.value){
+  if('undefied' !== typeof f_club.dataset.value){
     const c = JSON.parse(f_club.dataset.value);
     regatta.Club = c;
   }else{
